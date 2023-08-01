c9c1fe245add797c4dd4e31b1f69189d587a70bf
ArminPP <app@gmx.at>
2023-08-01 14:40:17 +0200

    demo of interrupt touch button & internal hall sensor!

--- src/XIAO_ESP32S3_EnergyMonitor.ORIGINAL.cpp
+++ src/XIAO_ESP32S3_EnergyMonitor.ORIGINAL.cpp
@@ -161,3 +161,74 @@ void loop()
         TriggerOnce = false; // after releasing the button, reset trigger for new button press event
     }
 }
+xxx
+
+/*
+
+This is an example how to use Touch Intrrerupts
+The sketh will tell when it is touched and then relesased as like a push-button
+
+This method based on touchInterruptGetLastStatus() is only available for ESP32 S2 and S3
+*/
+
+#include "Arduino.h"
+
+int threshold = 1500;   // ESP32S2 
+bool touch1detected = false;
+bool touch2detected = false;
+
+void gotTouch1() {
+  touch1detected = true;
+}
+
+void gotTouch2() {
+  touch2detected = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000); // give me time to bring up serial monitor
+
+  Serial.println("\n ESP32 Touch Interrupt Test\n");
+  touchAttachInterrupt(T1, gotTouch1, threshold); 
+  touchAttachInterrupt(T2, gotTouch2, threshold);
+}
+
+void loop() {
+  if (touch1detected) {
+    touch1detected = false;
+    if (touchInterruptGetLastStatus(T1)) {
+        Serial.println(" --- T1 Touched");
+    } else {
+        Serial.println(" --- T1 Released");
+    }
+  }
+  if (touch2detected) {
+    touch2detected = false;
+    if (touchInterruptGetLastStatus(T2)) {
+        Serial.println(" --- T2 Touched");
+    } else {
+        Serial.println(" --- T2 Released");
+    }
+  }
+
+  delay(80);
+}
+
+####################################################################################
+
+//Simple sketch to access the internal hall effect detector on the esp32.
+//values can be quite low. 
+//Brian Degger / @sctv  
+int val = 0;
+void setup() {
+  Serial.begin(9600);
+    }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = hallRead();
+  // print the results to the serial monitor:
+  //Serial.print("sensor = ");
+  Serial.println(val);//to graph 
+}
